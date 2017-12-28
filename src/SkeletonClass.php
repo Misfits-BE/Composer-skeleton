c11ccc4c4c21b09da28dda9da25f423d8611cd39
Tjoosten <Tjoosten3@gmail.com>
2017-12-28 14:17:34 +0100

    Initial commit

---
+++ src/SkeletonClass.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace League\Skeleton;
+
+class SkeletonClass
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    /**
+     * Friendly welcome
+     *
+     * @param  string $phrase Phrase to return
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase(string $phrase): string
+    {
+        return $phrase;
+    }
+}
